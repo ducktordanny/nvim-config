62f9e0f44509f914dfa3b27ae12122fe2229e56c
Daniel Lazar <lazardani0112@gmail.com>
2023-06-14 22:41:26 +0200

    Add legacy tag for figdet.nvim as it will be rewritten

--- lua/ducktordanny/plugins/init.lua
+++ lua/ducktordanny/plugins/init.lua
@@ -25,7 +25,7 @@ return {
       'williamboman/mason-lspconfig.nvim',
 
       -- Useful status updates for LSP
-      { 'j-hui/fidget.nvim', opts = {} },
+      { 'j-hui/fidget.nvim', opts = {}, tag = 'legacy' },
 
       -- Additional lua configuration, makes nvim stuff amazing!
       'folke/neodev.nvim',
