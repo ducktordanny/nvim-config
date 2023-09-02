818097ebdc13fc06e7b62cf59f0ae758a0ca28fe
Daniel Lazar <lazardani0112@gmail.com>
2023-09-02 21:49:29 +0200

    Remove bufferline as it feels unnecessary

--- lua/ducktordanny/remaps/basic.lua
+++ lua/ducktordanny/remaps/basic.lua
@@ -42,8 +42,8 @@ vim.keymap.set('n', '<leader>nr', ':NvimTreeRefresh<cr>', { desc = '[N]vimTree [
 vim.keymap.set('n', '<leader>W', ':bp|bd #<cr>', { desc = 'Close current buffer window and open previous' })
 vim.keymap.set('n', '<leader>WW', ':%bd|e#|bd#<cr>', { desc = 'Close all buffers but this' })
 vim.keymap.set('n', '<leader>WS', ':buffers<cr>:bd ', { desc = 'Select buffers to close' })
-vim.keymap.set('n', 'N', ':BufferLineCycleNext<cr>', { desc = 'BufferLine Cycle_[N]ext' })
-vim.keymap.set('n', 'P', ':BufferLineCyclePrev<cr>', { desc = 'BufferLine Cycle_[P]rev' })
+vim.keymap.set('n', 'N', ':bnext<cr>', { desc = 'buffer [n]ext' })
+vim.keymap.set('n', 'P', ':bprevious<cr>', { desc = 'buffer [p]revious' })
 
 -- git, gitsigns and vgit remaps
 vim.keymap.set('n', '<leader>gl', ':Flogsplit<cr>', { desc = '[G]it [L]og' })
