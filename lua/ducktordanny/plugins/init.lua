fb96a8e8e57f22ec4a5f0ef34800885a8f46d527
Daniel Lazar <lazardani0112@gmail.com>
2023-09-03 11:46:25 +0200

    Modify lualine filename symbols

--- lua/ducktordanny/plugins/init.lua
+++ lua/ducktordanny/plugins/init.lua
@@ -73,12 +73,26 @@ return {
   {
     -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
     -- See `:help lualine.txt`
     opts = {
       options = {
         theme = 'onedark',
         component_separators = { left = '', right = '|' },
         section_separators = '',
+      },
+      sections = {
+        lualine_c = {
+          {
+            'filename',
+            symbols = {
+              modified = '🟢',
+              readonly = '🟡',
+              unnamed = '⭕️',
+              newfile = '⚪️',
+            },
+          },
+        },
       },
     },
   },
