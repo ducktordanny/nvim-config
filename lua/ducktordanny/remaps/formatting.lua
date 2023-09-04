a4628441bfad4902f5881c3032f2d4be385f1eed
Daniel Lazar <lazardani0112@gmail.com>
2023-09-04 21:07:10 +0200

    Formatting: Refactor and remove unnecessary code parts

--- lua/ducktordanny/remaps/formatting.lua
+++ lua/ducktordanny/remaps/formatting.lua
@@ -1,41 +1,31 @@
 local prettier = require 'prettier'
+local ft = require 'guard.filetype'
+local guard = require 'guard'
+
+local filetypes = {
+  'css',
+  'graphql',
+  'html',
+  'javascript',
+  'javascriptreact',
+  'json',
+  'less',
+  'markdown',
+  'scss',
+  'typescript',
+  'typescriptreact',
+  'yaml',
+}
 
 prettier.setup {
   bin = 'prettier', -- or `'prettierd'` (v0.23.3+)
-  filetypes = {
-    'css',
-    'graphql',
-    'html',
-    'javascript',
-    'javascriptreact',
-    'json',
-    'less',
-    'markdown',
-    'scss',
-    'typescript',
-    'typescriptreact',
-    'yaml',
-  },
-  ['null-ls'] = {
-    condition = function()
-      return prettier.config_exists {
-        -- if `false`, skips checking `package.json` for `"prettier"` key
-        check_package_json = true,
-      }
-    end,
-    runtime_condition = function(params)
-      -- return false to skip running prettier
-      return true
-    end,
-    timeout = 5000,
-  },
+  filetypes,
 }
 
-local ft = require 'guard.filetype'
 ft('typescript,javascript,typescriptreact'):fmt 'prettier'
 ft('lua'):fmt 'stylua'
 
-require('guard').setup {
+guard.setup {
   -- the only options for the setup function
   fmt_on_save = true,
   -- Use lsp if no formatter was defined for this filetype
